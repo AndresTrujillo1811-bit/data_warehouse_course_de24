1c7260e28529ca45468f29f123fef80253df0010
AndresTrujillo1811-bit <andresgtrujillof@gmail.com>
2025-09-29 13:35:33 +0200

    fixing gitignore and definitions for dagster

---
+++ 15_orchestration_dagster_duckdb/orchestration/definitions.py
@@ -0,0 +1,14 @@
+import dlt
+import dagster as dg
+from dagster_dlt import DagsterDltResource, dlt_assets
+from dagster_dbt import DbtCliResource, dbt_assets, DbtProject
+from pathlib import Path
+
+# to import dlt script
+import sys 
+sys.path.insert(0, '../data_extract_load')
+from load_job_ads import jobads_resourse
+
+# data warehouse directory
+db_path = str(Path(__file__).parents[1] / "data_warehouse/job_ads.duckdb")
+
